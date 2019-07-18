cbb13353a4ca72a07fd7393a3ceeea92d73dcbe4
sansanda <david.sanchez@csic.es>
2019-07-18 16:47:48 +0200

    Power cycling 1.2.4 Addes GPIB Address treatatment and one more voltage channel for current measurement

--- Main/Power_cycling.py
+++ Main/Power_cycling.py
@@ -69,13 +69,16 @@ for line in initial_values:   #line will be each string in the previous array
         print('Total number of voltage channels:', voltageChannelsString)          #.replace because the split introduce a new line after the match 
         _channels = voltageChannelsString.split(":", -1)
         nVoltageChannels = int(_channels[1]) - int(_channels[0]) + 1
-        print(nVoltageChannels)
     if 'Temperature channels' in line:
         temperatureChannelsString = str(line[line.index('=')+1:]).replace("\n",'')     #Now, in the case of temperature
         print('Total number of temperature channels:', temperatureChannelsString)    #String similar to 1001:1012
         _channels = temperatureChannelsString.split(":", -1)    #Divide the string into: 1001 and 1012
-        nTemperatureChannels = int(_channels[1]) - int(_channels[0]) + 1    #Substract the two values in order to have the total number of temperature channels  
-        print(nTemperatureChannels)    
+        nTemperatureChannels = int(_channels[1]) - int(_channels[0]) + 1    #Substract the two values in order to have the total number of temperature channels    
+    if 'Current channel' in line:
+        currentChannelsString = str(line[line.index('=')+1:]).replace("\n",'')     #Now, in the case of current
+        print('Total number of current channels:', currentChannelsString)    #String similar to 1001:1001
+        _channels = currentChannelsString.split(":", -1)    #Divide the string into: 1001 and 1012
+        nCurrentChannels = int(_channels[1]) - int(_channels[0]) + 1    #Substract the two values in order to have the total number of temperature channels
     if 'Time transfer data' in line:
         t_transfer_data = float(line[line.index('=')+1:])     #The seconds elapsed from the electronic load starts NOT conducting till the data is transfered
         print('Moment to transfer the data (high and low measure):', t_transfer_data, 's')
@@ -85,6 +88,12 @@ for line in initial_values:   #line will be each string in the previous array
     if 'Csv file path' in line:
         csv_file_path = line[line.index('=')+1:]    #Relative Path to the CSV file (results)
         print('File path:', csv_file_path)
+    if 'Electronic Load GPIBAddr' in line:
+        electronic_load_GPIBAddr = int(line[line.index('=')+1:])    #Electronic load GPIB address
+        print('Electronic Load GPIBAddr:', electronic_load_GPIBAddr)
+    if 'Keithley 3706 GPIBAddr' in line:
+        multimeter_GPIBAddr = int(line[line.index('=')+1:])    #Keithley multimeter GPIB address
+        print('Keithley 3706 GPIBAddr:', multimeter_GPIBAddr)
 
 initial_values_f.close()    #In order to avoid errors, the file is closed
 print('\n---------------------------------------------------------------')
@@ -137,7 +146,7 @@ if check_time_parameters(t_on, t_off, t_measure_high, t_measure_low, t_transfer_
     exit()    #If any mentioned fact has happened, the program exits. 
 
 
-nChannels = nTemperatureChannels+nVoltageChannels    #Define the total number of channels
+nChannels = nTemperatureChannels + nVoltageChannels + nCurrentChannels      #Define the total number of channels (only one current channel)
 
 #CREATE AND OPEN THE CSV FILE TO TRANSFR THE ACQUIRED DATA
 field_names = ['Number of cycles', 'Semicycle', 'Time Stamp']    #First and constant headers
@@ -174,10 +183,10 @@ rm = visa.ResourceManager()
 
 
 #COMMUTICATE WITH THE ELECTRONIC LOAD 
-electronic_load = rm.open_resource('GPIB0::5::INSTR')   #Assign a variable to the Electronic load by its address
+electronic_load = rm.open_resource('GPIB0::'+str(electronic_load_GPIBAddr)+'::INSTR')   #Assign a variable to the Electronic load by its address
 
 #COMMUTICATE WITH KEITHLEY MULTIMETER
-multimeter = rm.open_resource('GPIB0::25::INSTR')   #Assign a variable to the multimeter by its address
+multimeter = rm.open_resource('GPIB0::'+str(multimeter_GPIBAddr)+'::INSTR')   #Assign a variable to the multimeter by its address
 
 
 #SENDING THE FIRST COMMANDS TO CONFIGURE THE ELECTRONIC LOAD
@@ -202,15 +211,19 @@ multimeter.write('dmm.func = dmm.DC_VOLTS')    #Set measurement function: Voltag
 multimeter.write('dmm.nplc=1')    #The integration rate in line cycles for the DMM for the function selected by dmm.func.
 multimeter.write('dmm.range=10')    #Set Range
 multimeter.write("dmm.configure.set('mydcvolts')")    #Save Configuration
-multimeter.write("dmm.setconfig('1001:10"+str(nChannels).zfill(2)+"','mydcvolts')")    #Assign configuration to channels
+multimeter.write("dmm.setconfig('"+voltageChannelsString+"','mydcvolts')")
+
+#Set current configuration (one voltage channel reserved to measure the current)
+multimeter.write("dmm.configure.set('mycurrent')")    #Save Configuration
+multimeter.write("dmm.setconfig('"+currentChannelsString+"','mycurrent')")
 
 
 #Set temperature configuration
 multimeter.write('dmm.func = "temperature"')    #Set measurement function: Temperature
 multimeter.write('dmm.transducer = dmm.TEMP_THERMOCOUPLE')    #Type of transducer: Thermocouple
 multimeter.write('dmm.thermocouple = dmm.THERMOCOUPLE_K')    #Type of thermocouple: K
-multimeter.write("dmm.configure.set('mythermocouple')")    #Save Configuration
-multimeter.write("dmm.setconfig('1013:1014','mythermocouple')")    #Assign configuration to channels
+multimeter.write("dmm.configure.set('mythermocouple')")    #Save Configuration   
+multimeter.write("dmm.setconfig('"+temperatureChannelsString+"','mythermocouple')")    #Assign configuration to channels
 
 
         
@@ -233,6 +246,7 @@ def prepare_Scan(_n_total_channels,_nScansPerSemicicle):
     #Creates an scan with a fixed number of voltage channels and adds a scan for the temperature channels
     multimeter.write("scan.create('"+voltageChannelsString+"','mydcvolts')")
     multimeter.write("scan.add('"+temperatureChannelsString+"','mythermocouple')")
+    multimeter.write("scan.add('"+currentChannelsString+"','mycurrent')")
     
     
     #Prepares the scan to be waiting for a trigger
