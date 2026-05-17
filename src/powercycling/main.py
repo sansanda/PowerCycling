56e7b650b72381d8a7e3a17e56a986ccc6a95fc8
sansanda <daolnoda@protonmail.com>
2026-05-17 02:10:44 +0200

    wip

--- src/powercycling/main.py
+++ src/powercycling/main.py
@@ -1,7 +1,12 @@
 from instruments.mocks.electronic_loads import FakeHPElectronicLoad6060B
+from instruments.interfaces.interfaces import ElectronicLoadInterface
+
+def depInjection(elI:ElectronicLoadInterface):
+    print(elI.mode)
 
 def main():
     el = FakeHPElectronicLoad6060B()
+    depInjection(el)
 
 
 if __name__ == "__main__":
